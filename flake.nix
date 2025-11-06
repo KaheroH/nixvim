42254a6f81a34120361972453f1d26b3bcce639d
Kahero Harriott <kharriot-ex@epichosted.com>
2025-11-06 14:29:00 -0600

    Template

--- flake.nix
+++ flake.nix
@@ -1,15 +1,43 @@
 {
-  description = "A very basic flake";
+  description = "Neovim configuration in nix using nixvim";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixvim.url = "github:nix-community/nixvim";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    self,
+    nixpkgs,
+    flake-parts,
+    nixvim,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkflake {inherit inputs;} {
+      imports = [];
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        nixvimLib = nixvim.lib.${system};
+        nixvim' = nixvim.laegacyPackages.${system};
+        nixvimModule = {
+          inherit pkgs;
+          module = import ./config;
+        };
+        nvim = nixvim'.makeNixvimWithModule nixvimModule;
+      in {
+        packages = {default = nvim;};
+      };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+      flake = {
+      };
+    };
 }
