edfcac0de9791da7226c7a65aaf4efd90625aa83
Kahero Harriott <kaheroharriott@gmail.com>
2025-11-22 15:28:49 -0600

    Returning the formatter

--- flake.nix
+++ flake.nix
@@ -42,6 +42,7 @@
           pre-commit-checks = pre-commit-hooks.lib.${system}.run {
             src = ./.;
             hooks = {
+              nixfmt-rfc-style.enable = true;
             };
           };
         };
