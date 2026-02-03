d42e4e58add214dce38275c09ffc443863b6ae32
Kahero Harriott <kharriot-ex@epichosted.com>
2026-02-03 11:45:33 -0600

    Startup Functional

--- config/plugins/ui/startup.nix
+++ config/plugins/ui/startup.nix
@@ -1,78 +1,77 @@
 {
-    plugin.startup = {
-        enable = true;
-        
-        colors = {
-            background = "#ffffff";
-            foldedSection = "#ffffff";
-        };
-
-        sections = {
-            header = {
-                type = "text";
-                oldfilesDirectory = false;
-                align = "center";
-                foldSection = false;
-                title = "Header";
-                margin = 5;
-                content = [
-                    " ██████╗░███████╗░█████╗░░█████╗░██████╗░████████╗░░░████████╗███████╗░█████╗░██╗░░██╗"
-                    " ██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝░░░╚══██╔══╝██╔════╝██╔══██╗██║░░██║"
-                    " ██║░░██║█████╗░░██║░░╚═╝██║░░██║██████╔╝░░░██║░░░░░░░░░██║░░░█████╗░░██║░░╚═╝███████║"
-                    " ██║░░██║██╔══╝░░██║░░██╗██║░░██║██╔══██╗░░░██║░░░░░░░░░██║░░░██╔══╝░░██║░░██╗██╔══██║"
-                    " ██████╔╝███████╗╚█████╔╝╚█████╔╝██║░░██║░░░██║░░░██╗░░░██║░░░███████╗╚█████╔╝██║░░██║"
-                    " ╚═════╝░╚══════╝░╚════╝░░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝"
-                ];
-                hightlight = "statement";
-                defaultColor = "";
-                oldfilesAmount = 0;
-            };
-
-            body = {
-                type = "mapping";
-                oldfilesDirectory = false;
-                alight = "center";
-                foldSection = false;
-                title = "Menu";
-                margin = 5;
-                content = [
-                    [
-                        " Find File"
-                        "Telescope find_files"
-                        "ff"
-                    ]
-                    [
-                        " Find Word"
-                        "Telescope live_grep"
-                        "fr"
-                    ]
-                    [
-                        " Recent Files"
-                        "Telescope oldfiles"
-                        "fg"
-                    ]
-                    [
-                        " File Browser"
-                        "Telescope file_browser"
-                        "fe"
-                    ]
-                ];
-                highlight = "string";
-                defaultColor = "";
-                oldfilesAmount = 0;
-            };
-        };
-
-        options = {
-            paddings = [
-                1
-                3
-            ];
-        };
-
-        parts = [
-            "header"
-            "body"
+  plugins.startup = {
+    enable = true;
+    settings = {
+      header = {
+        title = "Header";
+        type = "text";
+        oldfilesDirectory = false;
+        align = "center";
+        foldSection = false;
+        margin = 5;
+        content = [
+          "@@@  @@@  @@@@@@@@   @@@@@@   @@@  @@@  @@@  @@@@@@@@@@ "
+          "@@@@ @@@  @@@@@@@@  @@@@@@@@  @@@  @@@  @@@  @@@@@@@@@@@"
+          "@@!@!@@@  @@!       @@!  @@@  @@!  @@@  @@!  @@! @@! @@!"
+          "!@!!@!@!  !@!       !@!  @!@  !@!  @!@  !@!  !@! !@! !@!"
+          "@!@ !!@!  @!!!:!    @!@  !@!  @!@  !@!  !!@  @!! !!@ @!@"
+          "!@!  !!!  !!!!!:    !@!  !!!  !@!  !!!  !!!  !@!   ! !@!"
+          "!!:  !!!  !!:       !!:  !!!  :!:  !!:  !!:  !!:     !!:"
+          ":!:  !:!  :!:       :!:  !:!   ::!!:!   :!:  :!:     :!:"
+          " ::   ::   :: ::::  ::::: ::    ::::     ::  :::     :: "
+          "::    :   : :: ::    : :  :      :      :     :      :  "
         ];
+        highlight = "statement";
+        defaultColor = "";
+        oldfilesAmount = 0;
+      };
+      body = {
+        title = "Menu";
+        type = "mapping";
+        oldfilesDirectory = false;
+        align = "center";
+        foldSection = false;
+        margin = 5;
+        content = [
+          [
+            " Find File"
+            "Telescope find_files"
+            "ff"
+          ]
+          [
+            " Find Word"
+            "Telescope live_grep"
+            "fr"
+          ]
+          [
+            " Recent Files"
+            "Telescope oldfiles"
+            "fg"
+          ]
+          [
+            " File Browser"
+            "Telescope file_browser"
+            "fe"
+          ]
+        ];
+        highlight = "string";
+        defaultColor = "";
+        oldfilesAmount = 0;
+      };
+      colors = {
+        background = "#ffffff";
+        folded_Section = "#ffffff";
+      };
+      options = {
+        paddings = [
+          1
+          3
+        ];
+      };
+      parts = [
+        "header"
+        "body"
+      ];
     };
+  };
 }
